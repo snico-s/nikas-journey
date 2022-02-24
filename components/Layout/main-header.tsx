59c6efbbfc86b445ee959546acc512ec48c46762
Nico <nico.scheideler@gmail.com>
2022-02-24 17:19:33 +0100

    add: responsive navigation

--- components/Layout/main-header.tsx
+++ components/Layout/main-header.tsx
@@ -1,14 +1,77 @@
+import { useState, useRef, useEffect } from "react";
+
 import Link from "next/dist/client/link";
 import classes from "./main-header.module.css";
 
+import logo from "../../assets/nikas-journey-logo.png";
+import Image from "next/image";
+
 function MainHeader() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const menuSrOnlySpan = useRef<HTMLSpanElement>(null);
+
+  let resizeTimer: any;
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      document.body.classList.add("resize-animation-stopper");
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+      }, 400);
+    });
+  }, []);
+
   return (
-    <header className={classes.header}>
+    <header className={`${classes.primaryHeader} ${classes.flex}`}>
       <div className={classes.logo}>
-        <Link href="/">Nikas Journey</Link>
+        <Link href="/">
+          <Image
+            src="/nikas-journey-logo.png"
+            alt="Nikas Journey Logo"
+            width={48}
+            height={48}
+          />
+        </Link>
       </div>
-      <nav className={classes.navigation}>
-        <Link href="/ueber-uns">Über uns</Link>
+
+      <button
+        className={`${classes.mobileNavToggle} ${
+          isOpen ? classes.open : classes.close
+        }`}
+        aria-controls="primary-navigation"
+        onClick={() => {
+          setIsOpen((currentIsOpen) => !currentIsOpen);
+          if (menuSrOnlySpan.current) {
+            if (isOpen)
+              menuSrOnlySpan.current.setAttribute("aria-expanded", "true");
+            if (!isOpen)
+              menuSrOnlySpan.current.setAttribute("aria-expanded", "false");
+          }
+        }}
+      >
+        <span className="sr-only" aria-expanded="false" ref={menuSrOnlySpan}>
+          Menu
+        </span>
+      </button>
+
+      <nav>
+        <ul
+          id="primary-navigation"
+          data-visible="false"
+          className={`${classes.primaryNavigation} ${classes.flex} ${
+            isOpen ? classes.open : classes.close
+          }`}
+        >
+          <li className={`${classes.active}`}>
+            <Link href="/ueber-uns">Über uns</Link>
+          </li>
+          <li>
+            <Link href="/ueber-uns">Über uns</Link>
+          </li>
+          <li>
+            <Link href="/ueber-uns">Über uns</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
