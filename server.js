19151549932994782c1d01e40af1a6c7fe3278ff
Nico <nico.scheideler@gmail.com>
2022-04-04 11:39:19 +0200

    add: custom server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const next = require("next");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+nextApp.prepare().then(() => {
+  const app = express();
+
+  app.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
