07193936a353f5ede5403cd278726b74dac06d80
Nico <nico.scheideler@gmail.com>
2023-01-27 20:09:29 +0700

    move middleware

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -1,11 +1,11 @@
-import { NetConnectOpts } from "net";
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  //@ts-ignore
   const session = await getToken({ req, secret: process.env.SECRET });
 
+  console.log("middleware");
+  console.log(session);
   if (session?.isAdmin) {
     return NextResponse.next();
   }
